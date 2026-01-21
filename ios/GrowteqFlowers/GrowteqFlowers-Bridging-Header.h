c9bc7c4464fe0980e67e730ffe9794c223528435
ManuGA2612 <manugowda262000@gmail.com>
2026-01-21 15:06:53 +0530

    Add Swift bridging header to fix _DarwinFoundation3 module error in simulator build

---
+++ ios/GrowteqFlowers/GrowteqFlowers-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  GrowteqFlowers-Bridging-Header.h
+//
+
+#ifndef GrowteqFlowers_Bridging_Header_h
+#define GrowteqFlowers_Bridging_Header_h
+
+#import <React/RCTBridgeModule.h>
+#import <React/RCTViewManager.h>
+#import <React/RCTEventEmitter.h>
+
+// If you have any other Objective-C headers you need in Swift files, add them here
+
+#endif /* GrowteqFlowers_Bridging_Header_h */
