04cfe1f01f8307f1530b6285e172f579ac5e0ec5
ManuGA2612 <manugowda262000@gmail.com>
2026-01-22 15:51:52 +0530

    Update iOS AppDelegate and Codemagic build configuration

--- ios/GrowteqFlowers/AppDelegate.mm
+++ ios/GrowteqFlowers/AppDelegate.mm
@@ -1,34 +1,31 @@
 #import "AppDelegate.h"
 
-#import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
-#import <React/RCTRootView.h>
-#import <React/RCTAppDelegate.h>
-
-@interface AppDelegate () <RCTBridgeDelegate>
-
-@end
+#import <Firebase.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
+  // Initialize Firebase BEFORE React Native
+  if ([FIRApp defaultApp] == nil) {
+    [FIRApp configure];
+  }
 
-  RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
-                                                   moduleName:@"GrowteqFlowers"
-                                            initialProperties:nil];
+  self.moduleName = @"GrowteqFlowers";
+  // You can add your custom initial props in the dictionary below.
+  // They will be passed down to the ViewController used by React Native.
+  self.initialProps = @{};
 
-  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-  UIViewController *rootViewController = [UIViewController new];
-  rootViewController.view = rootView;
-  self.window.rootViewController = rootViewController;
-  [self.window makeKeyAndVisible];
-
-  return YES;
+  return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
+{
+  return [self bundleURL];
+}
+
+- (NSURL *)bundleURL
 {
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
